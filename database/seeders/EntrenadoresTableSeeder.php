72934b9889fe7140a22462ee7e93d83c0c6c2f5b
Damián Muñoz <damianmunoz4002@gmail.com>
2025-02-14 20:13:44 +0100

    Modificación 14/02/2025 20:13

--- database/seeders/EntrenadoresTableSeeder.php
+++ database/seeders/EntrenadoresTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EntrenadoresTableSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class EntrenadoresTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('entrenadores')->insert([
+            'correo' => 'admin@gmail.com',
+            'nombre' => 'admin',
+            'password' => 'admin',
+            'nacimiento' => '1970-01-01'
+        ]);
     }
 }
--- database/seeders/EquiposTableSeeder.php
+++ database/seeders/EquiposTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EquiposTableSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class EquiposTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('equipos')->insert([
+            'id_entrenador' => '1'
+        ]);
     }
 }
