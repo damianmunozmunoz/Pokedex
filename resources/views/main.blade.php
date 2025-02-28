e76400520635256652577da8eaca6d6d122e8102
rafamiralles <rafamiralles8@gmail.com>
2025-02-28 17:49:09 +0100

    Añadido cambio 32189391

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,31 +7,18 @@
 @section('content')
 <div class="d-grid gap-3 ">
     <form class="d-grid gap-3" action="{{ route('pokemons.index') }}" method="GET">
-<<<<<<< HEAD
         <input class="btn btn-primary btn-lg" type="submit" value="Pokemon">
-=======
-        <input class="btn btn-primary btn-lg text-center" type="submit" value="Pokemon">
->>>>>>> master
     </form>
     <form class="d-grid gap-3" action="{{ route('objetos.index') }}" method="GET">
         <input class="btn btn-primary btn-lg" type="submit" value="Objetos">
     </form>
-<<<<<<< HEAD
     <form class="d-grid gap-3" action="{{ route('tipos.index') }}" method="GET">
         <input class="btn btn-primary btn-lg" type="submit" value="Tipos">
     </form>
     
         
-=======
-    
-        <input class="btn btn-primary btn-lg" type="submit" value="Tipos">
->>>>>>> master
         <input class="btn btn-primary btn-lg" type="submit" value="Generaciones">
         <input class="btn btn-primary btn-lg" type="submit" value="Entrenadores">
         <input class="btn btn-primary btn-lg" type="submit" value="Equipos">
 </div>
-<<<<<<< HEAD
 @endsection
-=======
-@endsection
->>>>>>> master
